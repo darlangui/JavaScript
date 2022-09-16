bca8743d0428879162a66a1b6366591a415be5bd
Darlan <darlaneduardoguimaraes@gmail.com>
2022-09-16 17:11:59 -0300

    Last version JS Web - Mind

--- JSSonds/main.js
+++ JSSonds/main.js
@@ -1,18 +1,39 @@
-function Sons (element) {
-    document.querySelector(element).play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        //alert('Elemento não encontrado');
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
+
 }
 
-const listaTeclas = document.querySelectorAll('.tecla');
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let cont = 0;
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
-while (cont < listaTeclas.length){
-    const tecla = listaTeclas[cont];
-    const instru = tecla.classList[1];
-    
-    const audio = `#som_${instru}`;
-    console.log(tecla)
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
 
-    tecla.onclick = function(){Sons(audio)}
-    cont += 1;
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
 }
